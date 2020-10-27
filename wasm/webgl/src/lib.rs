d5b4339c2a97bff57ce03962a2791fc517d58a05
Antonio Caggiano <info@antoniocaggiano.eu>
2020-10-28 00:54:26 +0100

    Rust and WebGL: Draw a point
    Introduce vertex and fragment shaders by drawing a red point in the
    middle of the drawing area.

--- wasm/webgl/src/lib.rs
+++ wasm/webgl/src/lib.rs
@@ -19,19 +19,100 @@ macro_rules! log {
     }
 }
 
-/// Short WebGL program which simply clears a drawing area specified by a canvas tag
-#[wasm_bindgen]
-pub fn clear_drawing_area() -> Result<(), JsValue> {
+fn get_gl_context() -> Result<GL, JsValue> {
     utils::set_panic_hook();
 
     let doc = window().unwrap().document().unwrap();
     let canvas = doc.get_element_by_id("example").unwrap();
     let canvas: HtmlCanvasElement = canvas.dyn_into::<HtmlCanvasElement>()?;
 
-    let gl = canvas.get_context("webgl")?.unwrap().dyn_into::<GL>()?;
+    Ok(canvas.get_context("webgl")?.unwrap().dyn_into::<GL>()?)
+}
+
+/// Short WebGL program which simply clears a drawing area specified by a canvas tag
+#[wasm_bindgen]
+pub fn clear_drawing_area() -> Result<(), JsValue> {
+    let gl = get_gl_context()?;
 
     gl.clear_color(0.0, 0.0, 0.0, 1.0);
     gl.clear(GL::COLOR_BUFFER_BIT);
+
+    Ok(())
+}
+
+pub fn compile_shader(gl: &GL, shader_type: u32, source: &str) -> WebGlShader {
+    let shader = gl.create_shader(shader_type).unwrap();
+    gl.shader_source(&shader, source);
+    gl.compile_shader(&shader);
+
+    if !gl
+        .get_shader_parameter(&shader, GL::COMPILE_STATUS)
+        .as_bool()
+        .unwrap()
+    {
+        let msg = gl
+            .get_shader_info_log(&shader)
+            .unwrap_or_else(|| String::from("Unknown error"));
+        panic!("Failed to compile shader: {}", msg);
+    }
+
+    shader
+}
+
+fn link_program(gl: &GL, vert: WebGlShader, frag: WebGlShader) -> WebGlProgram {
+    let program = gl.create_program().unwrap();
+
+    gl.attach_shader(&program, &vert);
+    gl.attach_shader(&program, &frag);
+    gl.link_program(&program);
+
+    if !gl
+        .get_program_parameter(&program, GL::LINK_STATUS)
+        .as_bool()
+        .unwrap()
+    {
+        let msg = gl
+            .get_program_info_log(&program)
+            .unwrap_or_else(|| String::from("Unknown error"));
+        panic!("Failed to link program: {}", msg);
+    }
+
+    gl.delete_shader(Some(&vert));
+    gl.delete_shader(Some(&frag));
+
+    program
+}
+
+/// Draws a point in the middle of the drawing area
+#[wasm_bindgen]
+pub fn draw_point() -> Result<(), JsValue> {
+    let vert_source = r#"
+    void main() {
+        gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
+        gl_PointSize = 10.0;
+    }
+    "#;
+
+    let frag_source = r#"
+    void main() {
+        gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    }
+    "#;
+
+    let gl = get_gl_context()?;
+
+    let vert_shader = compile_shader(&gl, GL::VERTEX_SHADER, vert_source);
+    let frag_shader = compile_shader(&gl, GL::FRAGMENT_SHADER, frag_source);
+    let program = link_program(&gl, vert_shader, frag_shader);
+
+    gl.use_program(Some(&program));
+
+    gl.clear_color(0.0, 0.0, 0.0, 1.0);
+    gl.clear(GL::COLOR_BUFFER_BIT);
+
+    gl.draw_arrays(GL::POINTS, 0, 1);
+
+    gl.delete_program(Some(&program));
 
     Ok(())
 }
--- wasm/webgl/www/index.js
+++ wasm/webgl/www/index.js
@@ -1,3 +1,3 @@
 import * as webgl from "webgl";
 
-webgl.clear_drawing_area();
+webgl.draw_point();
