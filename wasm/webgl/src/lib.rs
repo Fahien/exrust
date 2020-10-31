1949f04f2538f074f5d449c57ca27ee291e69ad1
Antonio Caggiano <info@antoniocaggiano.eu>
2020-10-31 11:18:43 +0100

    Rust and WebGL: Improve point drawing
    Introduce attributes and uniforms by adding position, size and color to
    change how the point is rendered onto the drawing area.
    Also introduce a Context structure to keep track of both GL context and
    shader program avoiding to recreate them every time we draw.

--- wasm/webgl/src/lib.rs
+++ wasm/webgl/src/lib.rs
@@ -19,11 +19,12 @@ macro_rules! log {
     }
 }
 
+/// Returns a WebGL Context
 fn get_gl_context() -> Result<GL, JsValue> {
     utils::set_panic_hook();
 
     let doc = window().unwrap().document().unwrap();
-    let canvas = doc.get_element_by_id("example").unwrap();
+    let canvas = doc.get_element_by_id("area").unwrap();
     let canvas: HtmlCanvasElement = canvas.dyn_into::<HtmlCanvasElement>()?;
 
     Ok(canvas.get_context("webgl")?.unwrap().dyn_into::<GL>()?)
@@ -40,6 +41,7 @@ pub fn clear_drawing_area() -> Result<(), JsValue> {
     Ok(())
 }
 
+/// Compiles source code into a shader object
 pub fn compile_shader(gl: &GL, shader_type: u32, source: &str) -> WebGlShader {
     let shader = gl.create_shader(shader_type).unwrap();
     gl.shader_source(&shader, source);
@@ -59,6 +61,7 @@ pub fn compile_shader(gl: &GL, shader_type: u32, source: &str) -> WebGlShader {
     shader
 }
 
+/// Links vertex and fragment shader into a shader program
 fn link_program(gl: &GL, vert: WebGlShader, frag: WebGlShader) -> WebGlProgram {
     let program = gl.create_program().unwrap();
 
@@ -83,36 +86,62 @@ fn link_program(gl: &GL, vert: WebGlShader, frag: WebGlShader) -> WebGlProgram {
     program
 }
 
-/// Draws a point in the middle of the drawing area
 #[wasm_bindgen]
-pub fn draw_point() -> Result<(), JsValue> {
-    let vert_source = r#"
-    void main() {
-        gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
-        gl_PointSize = 10.0;
+pub struct Context {
+    gl: WebGlRenderingContext,
+    program: WebGlProgram,
+}
+
+#[wasm_bindgen]
+impl Context {
+    pub fn new() -> Result<Context, JsValue> {
+        let gl = get_gl_context()?;
+
+        let vert_source = r#"
+        attribute vec2 position;
+        attribute float point_size;
+
+        void main() {
+            gl_Position = vec4(position, 0.0, 1.0);
+            gl_PointSize = point_size;
+        }
+        "#;
+
+        let frag_source = r#"
+        precision mediump float;
+
+        uniform vec4 color;
+
+        void main() {
+            gl_FragColor = color;
+        }
+        "#;
+
+        let vert_shader = compile_shader(&gl, GL::VERTEX_SHADER, vert_source);
+        let frag_shader = compile_shader(&gl, GL::FRAGMENT_SHADER, frag_source);
+        let program = link_program(&gl, vert_shader, frag_shader);
+
+        Ok(Context { gl, program })
     }
-    "#;
 
-    let frag_source = r#"
-    void main() {
-        gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    /// Draws a point at position x and y
+    pub fn draw_point(&self, x: f32, y: f32) -> Result<(), JsValue> {
+        self.gl.use_program(Some(&self.program));
+
+        let position_loc = self.gl.get_attrib_location(&self.program, "position");
+        self.gl.vertex_attrib3f(position_loc as u32, x, y, 0.0);
+
+        let point_size_loc = self.gl.get_attrib_location(&self.program, "point_size");
+        self.gl.vertex_attrib1f(point_size_loc as u32, 16.0);
+
+        let color_loc = self.gl.get_uniform_location(&self.program, "color");
+        self.gl.uniform4f(color_loc.as_ref(), 0.0, 1.0, 0.0, 1.0);
+
+        self.gl.clear_color(0.0, 0.0, 0.0, 1.0);
+        self.gl.clear(GL::COLOR_BUFFER_BIT);
+
+        self.gl.draw_arrays(GL::POINTS, 0, 1);
+
+        Ok(())
     }
-    "#;
-
-    let gl = get_gl_context()?;
-
-    let vert_shader = compile_shader(&gl, GL::VERTEX_SHADER, vert_source);
-    let frag_shader = compile_shader(&gl, GL::FRAGMENT_SHADER, frag_source);
-    let program = link_program(&gl, vert_shader, frag_shader);
-
-    gl.use_program(Some(&program));
-
-    gl.clear_color(0.0, 0.0, 0.0, 1.0);
-    gl.clear(GL::COLOR_BUFFER_BIT);
-
-    gl.draw_arrays(GL::POINTS, 0, 1);
-
-    gl.delete_program(Some(&program));
-
-    Ok(())
 }
--- wasm/webgl/www/index.js
+++ wasm/webgl/www/index.js
@@ -1,3 +1,13 @@
 import * as webgl from "webgl";
 
-webgl.draw_point();
+var ctx = webgl.Context.new();
+
+var canvas = document.getElementById("area");
+canvas.onclick = (event) => {
+    const x = (event.clientX / canvas.clientWidth) * 2.0 - 1.0;
+    const y = - ((event.clientY / canvas.clientHeight) * 2.0 - 1.0);
+    console.log(x + " " + y);
+    ctx.draw_point(x, y);
+}
+
+ctx.draw_point(0.0, 0.0);
